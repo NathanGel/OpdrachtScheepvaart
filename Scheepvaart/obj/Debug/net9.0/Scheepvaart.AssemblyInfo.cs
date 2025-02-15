dfcc88d7e0e736a59f843f06df97f657934740b2
NathanGel <nathan.geleyn@student.hogent.be>
2025-02-15 20:13:26 +0100

    changes ti Tankers

--- Scheepvaart/obj/Debug/net9.0/Scheepvaart.AssemblyInfo.cs
+++ Scheepvaart/obj/Debug/net9.0/Scheepvaart.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Scheepvaart")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4b2a805ba1ed40abf42cc1e4e20c81d0b6322940")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3148de1a6a46e8ba3b42e19ba9fe54616ddeaf8d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Scheepvaart")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Scheepvaart")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
