0810f261b10f2b1b4071060db9670be563d73987
Noraelm <nora.elmoudni@student.hogent.be>
2025-02-16 15:53:53 +0100

    program klassen aangepast

--- Scheepvaart/obj/Debug/net9.0/Scheepvaart.AssemblyInfo.cs
+++ Scheepvaart/obj/Debug/net9.0/Scheepvaart.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Scheepvaart")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+50f35239f6f0d034d018bc39d77f96828351ac10")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d5c94868fff636332dab37deeee1914017346e06")]
 [assembly: System.Reflection.AssemblyProductAttribute("Scheepvaart")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Scheepvaart")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
